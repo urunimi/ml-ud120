b980f4f9a6fcde32b52c5acfe05636f25c300a09
Pyungwoo Yoo <hovan@hovans.com>
2019-09-30 10:24:50 +0900

    svm h/w

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,39 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#########################################################
+### your code goes here ###
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
 
+# from sklearn.svm import SVC
+# from sklearn.metrics import accuracy_score
 
+# clf = SVC(kernel="linear")
+# clf.fit(features_train, labels_train)
+# prd = clf.predict(features_test)
+# acc = accuracy_score(prd, labels_test)
+# print(acc)
+#########################################################
 
 #########################################################
 ### your code goes here ###
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(C=10000, kernel="rbf")
+clf.fit(features_train, labels_train)
+prd = clf.predict(features_test)
+acc = accuracy_score(prd, labels_test)
+print(acc)
+for i in [10, 26, 50]:
+    print('[{}]: {}'.format(i, prd[i]))
+
+from collections import Counter
+
+c = Counter(prd)
+print("No of predictions for Chris(1):", c[1])
 #########################################################
-
-
