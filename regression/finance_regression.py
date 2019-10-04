c79011f6069f6721c57ebae610fa9fe0cf5cd389
Pyungwoo Yoo <hovan@hovans.com>
2019-10-05 00:56:34 +0900

    regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,21 +17,21 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load(open("../final_project/final_project_dataset_modified.pkl", "rb"))
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+# features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"]
+# features_list = ["stock_options", "expences"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
@@ -40,12 +40,12 @@ test_color = "b"
 from sklearn.linear_model import LinearRegression
 
 reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
+print ('Slope - ', reg.coef_)
+print ('Intercept - ', reg.intercept_)
+print ('Score for training - ', reg.score(feature_train, target_train))
+print ('Score for testing - ', reg.score(feature_test, target_test))
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
@@ -55,16 +55,16 @@ for feature, target in zip(feature_train, target_train):
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
-plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
-
-
-
+plt.scatter(feature_train[0], target_train[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
-try:
-    plt.plot( feature_test, reg.predict(feature_test) )
-except NameError:
-    pass
+plt.plot(feature_test, reg.predict(feature_test), color= "g" )
+
+reg.fit(feature_test, target_test)
+
+plt.plot(feature_train, reg.predict(feature_train), color="y") 
+print("New Slope with testind data:", reg.coef_)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import pickle
-import _pickle as cPickle
+import pickle
 import numpy
 
-from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -29,14 +29,17 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    with open(authors_file, 'rb') as file:
-        authors = pickle.load(file)
-    with open(words_file, 'rb') as file:
-        word_data = pickle.load(file)
+    authors_file_handler = open(authors_file, "rb")
+    authors = pickle.load(authors_file_handler)
+    authors_file_handler.close()
+
+    words_file_handler = open(words_file, "rb")
+    word_data = pickle.load(words_file_handler)
+    words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
