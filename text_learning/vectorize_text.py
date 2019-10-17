49f20ffbb0ff3f1daa6c641ebfe57bfb53bf7cdb
Pyungwoo Yoo <hovan@hovans.com>
2019-10-17 10:07:10 +0900

    stopwords

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -22,6 +22,11 @@ from parse_out_email_text import parseOutText
     The data is stored in lists and packed away in pickle files at the end.
 """
 
+from nltk.corpus import stopwords
+
+sw = stopwords.words('english')
+print(len(sw))
+
 
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
@@ -44,8 +49,8 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+            print(path)
+            email = open(path, "rb")
 
             ### use parseOutText to extract the text from the opened email
 
@@ -59,12 +64,13 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             email.close()
 
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+
 
 
 
